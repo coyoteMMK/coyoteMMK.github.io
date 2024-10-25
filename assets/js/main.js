552daed9a33aa7786549b40b167e578c4bd2e75e
coyoteMMK <136488604+coyoteMMK@users.noreply.github.com>
2024-10-25 09:34:23 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,23 @@
+
 var aside_toggle = document.getElementById('aside-toggle');
 var aside = document.getElementById('aside-nav');
+var content = document.querySelector('.content'); // Selecciona el contenedor de contenido principal
 
 /** Función para mostrar/ocultar el menú lateral **/
 function toggleMenu() {
-    aside.classList.toggle('active'); // Alternar la clase 'active'
+    aside.classList.toggle('active'); // Alternar clase 'active' para el menú
 
     if (aside.classList.contains('active')) {
-        // Si el menú está activo, cambia el ícono del botón a "X"
+        // Si el menú está activo, añade clase al contenido para ajustar el margen
+        content.classList.add('menu-visible');
         aside_toggle.innerHTML = '<i class="bx bx-x"></i>';
     } else {
-        // Si el menú está oculto, cambia el ícono del botón a "Menú"
+        // Si el menú está oculto, quita la clase al contenido
+        content.classList.remove('menu-visible');
         aside_toggle.innerHTML = '<i class="bx bx-menu"></i>';
     }
 }
+
 
 // Cargar imágenes de fondo dinámicamente cuando se carga la página
 window.addEventListener('load', function() {
