3c3f636fa980ea33f11484fc69405e09de1629e4
coyoteMMK <136488604+coyoteMMK@users.noreply.github.com>
2024-10-25 11:22:37 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,32 +1,65 @@
+/**
+ * Aside show and hide when screen_width = 1024px
+ */
 var aside_toggle = document.getElementById('aside-toggle');
-
+/** Aside show and hide when screen_width = 1024px **/
 function toggleMenu() {
     var aside = document.getElementById('aside-nav');
-    aside.classList.toggle('active'); // Activa o desactiva la clase 'active'
-
+    aside.classList.toggle('active');
     if (aside.classList.contains('active')) {
         aside_toggle.innerHTML = '<i class="bx bx-x"></i>';
+        aside.animate([
+            { transform: 'translateX(-100%)' },
+            { transform: 'translateX(0)' }
+        ], {
+            duration: 400,
+            fill: 'forwards'
+        });
+        aside_toggle.animate([
+            { opacity: '0' },
+            { opacity: '1' }
+        ], {
+            duration: 400,
+            fill: 'forwards'
+        });
+
         aside.style.display = 'block';
-    } else {
+    }else if (!aside.classList.contains('active')) {
         aside_toggle.innerHTML = '<i class="bx bx-menu"></i>';
-        setTimeout(() => {
-            aside.style.display = 'none';
-        }, 400); // Ocultar después de la animación
+        aside.animate([
+            { transform: 'translateX(0)' },
+            { transform: 'translateX(-100%)' }
+        ], {
+            duration: 400,
+            fill: 'forwards'
+        });
+        aside_toggle.animate([
+            { opacity: '0' },
+            { opacity: '1' }
+        ], {
+            duration: 400,
+            fill: 'forwards'
+        });
     }
 }
 
-// Detectar el cambio de tamaño de ventana
 window.addEventListener('resize', function() {
     var aside = document.getElementById('aside-nav');
-
-    // Resetear el menú en pantallas grandes
-    if (window.innerWidth > 1024) {
-        aside.classList.remove('active'); 
-        aside.style.display = 'none';
-        aside_toggle.innerHTML = '<i class="bx bx-menu"></i>';
+    var aside_toggle = document.getElementById('aside-toggle');
+    if (window.innerWidth > 1024) { // Verificamos el ancho de la pantalla
+        if (!aside.classList.contains('active')) { // Si el menu está cerrado
+            aside_toggle.innerHTML = '<i class="bx bx-menu"></i>'; // Cambiamos el icono del boton
+        }
+        aside.animate([
+            { transform: 'translateX(-100%)' },
+            { transform: 'translateX(0%)' }
+        ], {
+            duration: 0,
+            fill: 'forwards'
+        });
+        
     }
 });
-
 
 window.addEventListener('load', function() {
     var bg = document.getElementsByClassName('bg');
